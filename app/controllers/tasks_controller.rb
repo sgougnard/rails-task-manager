4796a2f4afd982500b7b3a6f437fab773ea6812b
Simon Gougnard <simongougnard@gmail.com>
2020-06-09 16:57:31 +0200

    added actions create show et index

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,37 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+
+  def new
+   @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+      # SEE ALL TASKS
+  get "tasks" => "tasks#index", as: :tasks
+
+
+  # CREATE A NEW TASK (2 HTTP REQUESTS)
+  get "tasks/new" => "tasks#new"
+  post "tasks" => "tasks#create"
+
+  # SEE ONE PARTICULAR TASK
+  get "tasks/:id" => "tasks#show", as: :task
+
+  # EDIT & UPDATE A RESTAURANT (Technically “edit then update”)
+  get "tasks/:id/edit" => "tasks#edit"
+  patch "tasks/:id" => "tasks#update"
+
+  # DELETE A RESTAURANT (in this case, destroy)
+  delete "tasks/:id"  => "tasks#destroy"
+  # CHEATCODE:
+  # resources: Tasks
 end
